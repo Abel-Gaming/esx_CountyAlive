e8467fbc2010d45733cdd2bb55a2c2c945b1f58c
Abel Gaming <lcpdpolice4939@gmail.com>
2021-06-23 15:49:07 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -75,10 +75,14 @@ Config.FoodVendors = {
 -- Camping
 Config.EnableCamping = true -- Allow players to setup camp and experience the great outdoors!
 
-Config.TentModelHash = 	-596943609
+Config.TentModelHash = -596943609
 Config.CampFireModelHash = -1065766299
-Config.GrillModelHash =  	977744387 -- Find more here: http://www.test.raccoon72.ru/?s=bbq
+Config.GrillModelHash =  977744387 -- Find more here: http://www.test.raccoon72.ru/?s=bbq
+Config.ChairModelHash = 1071807406
 
+Config.UseCommands = false -- If set to false, the menu will be used with all functions inside of it
+
+Config.CampingMenuCommand = 'camping-menu'
 Config.StartCampfireCommand = 'set-campfire'
 Config.PitchTentCommand = 'set-tent'
 Config.PlaceGrillCommand = 'set-grill'
